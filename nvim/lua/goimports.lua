c28e20df3b8e44518ee0d5044859bce954b24552
edstell <edward.j.stell@gmail.com>
2024-07-22 19:09:23 +0100

    move lsp config into other files

---
+++ nvim/lua/goimports.lua
@@ -0,0 +1,16 @@
+-- Organise imports like goimports
+-- https://github.com/golang/tools/blob/master/gopls/doc/vim.md#imports
+function goimports(wait_ms)
+	local params = vim.lsp.util.make_range_params()
+	params.context = {only = {"source.organizeImports"}}
+	local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
+	for _, res in pairs(result or {}) do
+		for _, r in pairs(res.result or {}) do
+			if r.edit then
+				vim.lsp.util.apply_workspace_edit(r.edit, "utf-16")
+			else
+				vim.lsp.buf.execute_command(r.command)
+			end
+		end
+	end
+end
